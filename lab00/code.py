67e75440cec45697579d4f3db85c32764f1436b8
skateboardant <ley27510@gmail.com>
2026-05-22 23:32:18 +0800

    modification for 15 case

--- lab00/code.py
+++ lab00/code.py
@@ -7,6 +7,9 @@ def get_airspeed_velocity_of(unladen_swallow):
 # pretend there's code here...
 
 def fizzbuzz(num):
+  if (num % 3 == 0) and (num % 5 == 0)
+    print(f"{num}: fizzbuzz")
+    return
   if num % 3 == 0: # edit this line
     print(f"{num}: fizz")
   if num % 5 == 0: # edit this line
